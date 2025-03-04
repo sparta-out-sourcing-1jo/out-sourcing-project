8b649f75efed3e73f826c3bd88b7383543b963cf
cheoni5417 <cheoni5417@naver.com>
2025-03-04 14:56:32 +0900

    Refactor : JwtUtil 충돌 방지

--- src/main/java/com/example/outsourcing/common/util/JwtUtil.java
+++ src/main/java/com/example/outsourcing/common/util/JwtUtil.java
@@ -1,21 +1,4 @@
 package com.example.outsourcing.common.util;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import org.springframework.stereotype.Component;
-
-import java.security.Key;
-
-@Component
 public class JwtUtil {
-
-    private Key key;
-
-    public Claims extractClaims(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
 }
